96a4997801682d690d4194d42274dbc2786b9be1
Jalp Hemantkumar Bhatt <Jalp_Bhatt@ad.infosys.com>
2018-10-03 08:34:55 +0530

    insersion queries with mock data

--- insert_table_query.sql
+++ insert_table_query.sql
@@ -5,9 +5,35 @@ INSERT INTO `branch_visit_db`.`branch`
 `branch_desc`,
 `branch_name`)
 VALUES
-(<{branch_code: }>,
-<{branch_desc: }>,
-<{branch_name: }>);
+("035-432", "Westpac brach of Melbourne", "Victoria Harbour");
+
+INSERT INTO `branch_visit_db`.`branch`
+(`branch_code`,
+`branch_desc`,
+`branch_name`)
+VALUES
+("032-712", "Westpac brach of Belconnen", "Belconnen");
+
+INSERT INTO `branch_visit_db`.`branch`
+(`branch_code`,
+`branch_desc`,
+`branch_name`)
+VALUES
+("732-712", "Westpac brach of Canberra", "53 Alinga Street, Canberra");
+
+INSERT INTO `branch_visit_db`.`branch`
+(`branch_code`,
+`branch_desc`,
+`branch_name`)
+VALUES
+("732-001", "Westpac brach of Melbourne", "Albury");
+
+INSERT INTO `branch_visit_db`.`branch`
+(`branch_code`,
+`branch_desc`,
+`branch_name`)
+VALUES
+("033-003", "Westpac brach of Melbourne", "114 William St");
 
 -- 2. Contractor Table insert query
 
@@ -63,18 +89,50 @@ INSERT INTO `branch_visit_db`.`document`
 `doc_path`,
 `doc_type_id`)
 VALUES
-(<{doc_id: }>,
-<{doc_path: }>,
-<{doc_type_id: }>);
+(1, "C:/fakepath/1.png","1");
+
+INSERT INTO `branch_visit_db`.`document`
+(`doc_id`,
+`doc_path`,
+`doc_type_id`)
+VALUES
+(2, "C:/fakepath/1.jpeg","2");
+
+INSERT INTO `branch_visit_db`.`document`
+(`doc_id`,
+`doc_path`,
+`doc_type_id`)
+VALUES
+(3, "C:/fakepath/1.pdf","3");
+
+INSERT INTO `branch_visit_db`.`document`
+(`doc_id`,
+`doc_path`,
+`doc_type_id`)
+VALUES
+(4, "C:/fakepath/1.xlsx","4");
 
 -- 5. Document Type Table insert query
 
 INSERT INTO `branch_visit_db`.`document_type`
 (`doc_type_id`,
 `doc_type`)
-VALUES
-(<{doc_type_id: }>,
-<{doc_type: }>);
+VALUES ("1", "png");
+
+INSERT INTO `branch_visit_db`.`document_type`
+(`doc_type_id`,
+`doc_type`)
+VALUES ("2", "jpeg");
+
+INSERT INTO `branch_visit_db`.`document_type`
+(`doc_type_id`,
+`doc_type`)
+VALUES ("3", "pdf");
+
+INSERT INTO `branch_visit_db`.`document_type`
+(`doc_type_id`,
+`doc_type`)
+VALUES ("3", "xlsx");
 
 -- 6. Employee Table insert query
 
@@ -87,13 +145,51 @@ INSERT INTO `branch_visit_db`.`employee`
 `branch_code`,
 `role_type`)
 VALUES
-(<{employee_id: }>,
-<{employee_auth_token: }>,
-<{employee_name: }>,
-<{password: }>,
-<{username: }>,
-<{branch_code: }>,
-<{role_type: }>);
+(654768, "uuid", "William", "", "William","033-003","AUT");
+
+INSERT INTO `branch_visit_db`.`employee`
+(`employee_id`,
+`employee_auth_token`,
+`employee_name`,
+`password`,
+`username`,
+`branch_code`,
+`role_type`)
+VALUES
+(654768, "uuid", "William", "", "William","732-001","AUT");
+
+INSERT INTO `branch_visit_db`.`employee`
+(`employee_id`,
+`employee_auth_token`,
+`employee_name`,
+`password`,
+`username`,
+`branch_code`,
+`role_type`)
+VALUES
+(654768, "uuid", "William", "", "William","732-712","AUT");
+
+INSERT INTO `branch_visit_db`.`employee`
+(`employee_id`,
+`employee_auth_token`,
+`employee_name`,
+`password`,
+`username`,
+`branch_code`,
+`role_type`)
+VALUES
+(654768, "uuid", "William", "", "John","035-432","SU");
+
+INSERT INTO `branch_visit_db`.`employee`
+(`employee_id`,
+`employee_auth_token`,
+`employee_name`,
+`password`,
+`username`,
+`branch_code`,
+`role_type`)
+VALUES
+(654768, "uuid", "William", "", "John","032-712","SU");
 
 -- 7. Job Type Table insert query
 
@@ -102,9 +198,42 @@ INSERT INTO `branch_visit_db`.`job_type`
 `job_desc`,
 `job_type_name`)
 VALUES
-(<{job_code: }>,
-<{job_desc: }>,
-<{job_type_name: }>);
+("1","Air Conditioning Repair","Air Conditioning Repair");
+
+INSERT INTO `branch_visit_db`.`job_type`
+(`job_code`,
+`job_desc`,
+`job_type_name`)
+VALUES
+("2","Lighting Installation","Lighting Installation");
+
+INSERT INTO `branch_visit_db`.`job_type`
+(`job_code`,
+`job_desc`,
+`job_type_name`)
+VALUES
+("3","Fire Alarm Repair","Fire Alarm Repair");
+
+INSERT INTO `branch_visit_db`.`job_type`
+(`job_code`,
+`job_desc`,
+`job_type_name`)
+VALUES
+("4","Plumbing work","Plumbing work");
+
+INSERT INTO `branch_visit_db`.`job_type`
+(`job_code`,
+`job_desc`,
+`job_type_name`)
+VALUES
+("5","Fire system inspections","Fire system inspections");
+
+INSERT INTO `branch_visit_db`.`job_type`
+(`job_code`,
+`job_desc`,
+`job_type_name`)
+VALUES
+("6","ATM repair","ATM repair");
 
 -- 8. Role Type Table insert query
 
@@ -113,11 +242,18 @@ INSERT INTO `branch_visit_db`.`role_type`
 `role_desc`,
 `role_name`)
 VALUES
-(<{role_code: }>,
-<{role_desc: }>,
-<{role_name: }>);
+("AUT","Bank employee with limited branch acess","Authoriser");
+
+INSERT INTO `branch_visit_db`.`role_type`
+(`role_code`,
+`role_desc`,
+`role_name`)
+VALUES
+("SU","Bank employee with all branch acess","Super User");
 
 -- 9. Vendor Table insert query
+
+-- TODO: Remove contractor id from here as we have vendor id as foriegn key in contractor table
 
 INSERT INTO `branch_visit_db`.`vendor`
 (`vendor_id`,
@@ -125,7 +261,28 @@ INSERT INTO `branch_visit_db`.`vendor`
 `vendor_name`,
 `contractor_id`)
 VALUES
-(<{vendor_id: }>,
-<{vendor_desc: }>,
-<{vendor_name: }>,
-<{contractor_id: }>);
+(1,"ABC Vendors","ABC Vendors","");
+
+INSERT INTO `branch_visit_db`.`vendor`
+(`vendor_id`,
+`vendor_desc`,
+`vendor_name`,
+`contractor_id`)
+VALUES
+(2,"Fixwell Vendors","Fixwell Vendors","");
+
+INSERT INTO `branch_visit_db`.`vendor`
+(`vendor_id`,
+`vendor_desc`,
+`vendor_name`,
+`contractor_id`)
+VALUES
+(3,"Fire Safe Vendors","Fire Safe Vendors","");
+
+INSERT INTO `branch_visit_db`.`vendor`
+(`vendor_id`,
+`vendor_desc`,
+`vendor_name`,
+`contractor_id`)
+VALUES
+(3,"ATM Professionals","ATM Professionals","");
